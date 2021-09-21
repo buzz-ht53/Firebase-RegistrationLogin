6d7e40f5736e80d1080a3043bbc9ea9866916cec
buzz-ht53 <akshayr.java@gmail.com>
2021-09-21 16:54:18 +0530

    First Commit

---
+++ app/src/main/java/com/buzz_ht/firebaselogin/LoginActivity.java
@@ -0,0 +1,88 @@
+package com.buzz_ht.firebaselogin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button buttonlogin;
+    EditText email,password;
+    FirebaseAuth firebaseAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        buttonlogin = findViewById(R.id.buttonlogin);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        buttonlogin = findViewById(R.id.buttonlogin);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+        buttonlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String EMAIL = email.getText().toString();
+                String PASS= password.getText().toString();
+
+                if(EMAIL.isEmpty()){
+                    email.setError("Email cant be black");
+                    email.requestFocus();
+                    return;
+
+                }
+
+                if(PASS.isEmpty()){
+                    password.setError("Password cannot be empty");
+                    password.requestFocus();
+                    return;
+                }
+
+                firebaseAuth.signInWithEmailAndPassword(EMAIL,PASS).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+                        }
+                        }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+
+            }
+        });
+
+
+
+
+    }
+
+    public void goback(View view) {
+
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+    }
+}
---
+++ app/src/main/java/com/buzz_ht/firebaselogin/MainActivity.java
@@ -0,0 +1,89 @@
+package com.buzz_ht.firebaselogin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText email,password;
+    Button buttonreg;
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        buttonreg = findViewById(R.id.buttonreg);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        buttonreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String EMAIL = email.getText().toString();
+                String PASS= password.getText().toString();
+
+                if(EMAIL.isEmpty()){
+                    email.setError("Email cant be black");
+                    email.requestFocus();
+                    return;
+
+                }
+
+                if(PASS.isEmpty()){
+                    password.setError("Password cannot be empty");
+                    password.requestFocus();
+                    return;
+                }
+
+                firebaseAuth.createUserWithEmailAndPassword(EMAIL,PASS).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(),"Registration Successful", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+
+
+            }
+
+
+
+        });
+
+
+
+
+
+    }
+
+    public void login(View view) {
+
+        startActivity(new Intent(MainActivity.this,LoginActivity.class));
+    }
+}
